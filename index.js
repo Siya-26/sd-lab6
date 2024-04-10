a266bd1d77c032043118ce862e80002059f1b03e
Siya-26 <2164586@students.wits.ac.za>
2024-04-10 20:00:04 +0200

    added process.env.PORT

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ app.post('/cars', (req, res) => {
 });
 
 //start app at localhost:3001
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log('Server started at http://localhost:3001');
 });
